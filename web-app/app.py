6f017c40da214795f0c41745a8adc491989510cc
Patrick Zhao <106126435+PatrickZhao0@users.noreply.github.com>
2023-12-01 22:08:45 -0500

    hotfix

--- web-app/app.py
+++ web-app/app.py
@@ -5,9 +5,8 @@ import base64
 
 
 app = Flask(__name__)
-#uri = os.getenv('MONGODB_URI')
-uri = "mongodb+srv://admin:admin123@cluster0.m5t5gvu.mongodb.net/?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
-connection = MongoClient(uri, server_api=ServerApi('1'))
+uri = "mongodb+srv://admin:admin123@cluster0.m5t5gvu.mongodb.net/?retryWrites=true&w=majority"
+connection = MongoClient(uri)
 db = connection["note_app"]
 notes = db.notes
 temp = db.temp
