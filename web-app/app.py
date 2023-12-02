013522f33f950d31fcf66245ab7ad35a7f5d2de0
Patrick Zhao <106126435+PatrickZhao0@users.noreply.github.com>
2023-12-02 00:59:02 -0500

    minor change to URI

--- web-app/app.py
+++ web-app/app.py
@@ -5,6 +5,7 @@ import base64
 
 
 app = Flask(__name__)
+#uri = os.getenv('MONGODB_URI')
 uri = "mongodb+srv://admin:admin123@cluster0.m5t5gvu.mongodb.net/?retryWrites=true&w=majority"
 connection = MongoClient(uri)
 db = connection["note_app"]
